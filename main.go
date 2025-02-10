ae89789c364bd29e9a437f6f7da883afc1a37262
Elias  Valdez <elias.vc@gmail.com>
2025-02-10 13:51:47 -0500

    Add 'snippet/create' and 'snippet/view' handlers

--- main.go
+++ main.go
@@ -5,9 +5,19 @@ import (
 	"net/http"
 )
 
-// Handler. This is in charge of writing a response when a client request wants "/"
+// Handler. This is in charge of writing a response when a client request wants "/" and everything else
+// Turns out "/" in the servermux world is a catch-all: it will catch every request that
+// doesn't have a handler
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet.."))
 }
 
 func main() {
@@ -15,6 +25,8 @@ func main() {
 	// of routing requests to the correct handler based on which resource is requested
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("starting server on :4000")
 
