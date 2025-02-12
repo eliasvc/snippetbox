51d6a501b86b806ba85f0733bdfdfd263cbc9aa7
Elias  Valdez <elias.vc@gmail.com>
2025-02-11 22:44:35 -0500

    Add home template file and display it in home handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"html/template"
+	"log"
 	"net/http"
 	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
-	io.WriteString(w, "Hello from Snippetbox")
+	// Parse template into template set
+	ts, err := template.ParseFiles("./ui/html/pages/home.tmpl")
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
 
 func snippetView(w http.ResponseWriter, r *http.Request) {
