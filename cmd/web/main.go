5f6f5ad46f21cb775bfcda5f0c59c887859c9d9b
Elias  Valdez <elias.vc@gmail.com>
2025-02-20 21:42:46 -0500

    Added static files and quick handler to take care of those

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,6 +8,24 @@ import (
 func main() {
 	// Router
 	mux := http.NewServeMux()
+
+	// Handle that creates a file server that will serve the contents
+	// of the directory ./ui/static. Note the relative path to the
+	// project
+	fileServer := http.FileServer(http.Dir("./ui/static"))
+	// http.FileServer will remove the leading "/" from the URL that's
+	// passed along and then search for the requested file by concatenating
+	// the URL and the directory. For example, if the request is:
+	// http://localhost:4000/css/boom.css
+	// The resulting search path will be ./ui/static/css/boom.css
+	// If on the other hand, the URL is:
+	// http://localhost:4000/static/css/boom.css
+	// The resulting search path will be ./ui/static/static/css/boom.css
+	// which does not exist. So we need to remove the extra "/static"
+	// before it's handled, which http.StripPrefix does
+	// http.StripPrefix returns another handler but one that has whatever
+	// prefix is specified before handling its business
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.HandleFunc("GET /{$}", home)
 	// Displays a specific snippet
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
